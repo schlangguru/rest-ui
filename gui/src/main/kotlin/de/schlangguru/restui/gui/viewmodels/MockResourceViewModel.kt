4a39b3bf562b091e01684c4f6d17373e92e333ca
Schlangguru <s.seidl@protonmail.com>
2018-05-10 15:02:47 +0200

    Implemented View for MockResource Responses

--- gui/src/main/kotlin/de/schlangguru/restui/gui/viewmodels/MockResourceViewModel.kt
+++ gui/src/main/kotlin/de/schlangguru/restui/gui/viewmodels/MockResourceViewModel.kt
@@ -2,14 +2,25 @@ package de.schlangguru.restui.gui.viewmodels
 
 import de.schlangguru.restui.app.model.MockResource
 import de.schlangguru.restui.app.model.MockResourceResponse
+import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleListProperty
+import javafx.beans.property.SimpleObjectProperty
 import tornadofx.ItemViewModel
 import tornadofx.observable
 
 class MockResourceViewModel: ItemViewModel<MockResource>() {
+
     val path = bind (MockResource::path)
     val method = bind (MockResource::method)
     val responses = bind { SimpleListProperty<MockResourceResponse>(item?.responses?.observable()) }
 
+    val selectedResponse = SimpleObjectProperty<MockResourceResponse>()
     val availableMethods = listOf("GET", "POST", "PUT", "DELETE", "HEAD").observable()
+}
+
+class MockResourceResponseViewModel: ItemViewModel<MockResourceResponse>() {
+    val name = bind (MockResourceResponse::name)
+    val statusCode = bind { SimpleIntegerProperty(item?.statusCode ?: 0) }
+    val contentType = bind (MockResourceResponse::contentType)
+    val content = bind (MockResourceResponse::content)
 }
--- gui/src/main/kotlin/de/schlangguru/restui/gui/views/MockResourceDetails.kt
+++ gui/src/main/kotlin/de/schlangguru/restui/gui/views/MockResourceDetails.kt
@@ -1,15 +1,11 @@
 package de.schlangguru.restui.gui.views
 
 import de.schlangguru.restui.app.model.MockResourceResponse
-import de.schlangguru.restui.app.model.Request
-import de.schlangguru.restui.gui.viewmodels.RequestViewModel
-import de.schlangguru.restui.app.model.RequestHeader
+import de.schlangguru.restui.gui.viewmodels.MockResourceResponseViewModel
 import de.schlangguru.restui.gui.viewmodels.MockResourceViewModel
-import de.schlangguru.restui.gui.viewmodels.RequestHeaderViewModel
-import javafx.geometry.Insets
-import javafx.geometry.Orientation
+import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
-import javafx.scene.text.FontWeight
+import javafx.stage.StageStyle
 import javafx.util.converter.NumberStringConverter
 import tornadofx.*
 
@@ -17,6 +13,7 @@ class MockResourceDetails : View() {
     override val root = VBox()
 
     private val viewModel: MockResourceViewModel by inject()
+    private val responseViewModel: MockResourceResponseViewModel by inject()
 
     init {
         with(root) {
@@ -34,11 +31,55 @@ class MockResourceDetails : View() {
 
                 fieldset("Responses") {
                     tableview(viewModel.responses) {
-                        smartResize()
                         readonlyColumn("Name", MockResourceResponse::name)
                         readonlyColumn("Status", MockResourceResponse::statusCode)
                         readonlyColumn("Content-Type", MockResourceResponse::contentType)
                         readonlyColumn("Entity", MockResourceResponse::content)
+
+                        smartResize()
+                        bindSelected(responseViewModel)
+                        onUserSelect { find(MockResourceResponseDetails::class).openModal(stageStyle = StageStyle.UTILITY) }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class MockResourceResponseDetails: Fragment() {
+    override val root = VBox()
+    private val viewModel: MockResourceResponseViewModel by inject()
+
+    init {
+        with (root) {
+            toolbar {
+                pane {
+                    hgrow = Priority.ALWAYS
+                }
+                button{
+                    tooltip("Reset")
+                    imageview("/icons/undo.png")
+                    action { viewModel.rollback() }
+                    enableWhen(viewModel.dirty)
+                }
+                button {
+                    tooltip("Save")
+                    imageview("/icons/checkmark.png")
+                    action { viewModel.commit() }
+                    enableWhen(viewModel.dirty)
+                }
+            }
+
+            form {
+                fieldset("Response: ${viewModel.name.value}") {
+                    field("Status Code:") {
+                        textfield(viewModel.statusCode, NumberStringConverter("#"))
+                    }
+                    field("Content Type: ") {
+                        textfield(viewModel.contentType)
+                    }
+                    field("Entity: ") {
+                        textarea(viewModel.content)
                     }
                 }
             }
